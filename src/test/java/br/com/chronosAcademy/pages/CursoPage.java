3f427217802aff08daf26ae5aae82f1877e33642
jonathanestrela <jonathanestrela@outlook.com>
2021-09-09 20:46:13 -0300

    Aula 10 PageFactory

--- src/test/java/br/com/chronosAcademy/pages/CursoPage.java
+++ src/test/java/br/com/chronosAcademy/pages/CursoPage.java
@@ -1,20 +1,25 @@
 package br.com.chronosAcademy.pages;
 
+import br.com.chronosAcademy.maps.CursoMap;
+import br.com.chronosAcademy.maps.PrincipalMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class CursoPage {
     private WebDriver driver;
+    private CursoMap cursoMap;
 
     public CursoPage(WebDriver driver) {
         this.driver = driver;
+        cursoMap = new CursoMap();
+        PageFactory.initElements(driver, cursoMap);
     }
 
     public String getTitulo2() {
-        String xpathtitulo = "//section[2]//h2";
-        WebElement h2Titulo = driver.findElement(By.xpath(xpathtitulo));
-        return h2Titulo.getText();
+
+        return cursoMap.h2Titulo.getText();
 
     }
 }
